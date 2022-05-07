5189830dd0fab4c551d0d5f11248dae4c2a6c996
PedroIvo00 <pedroivo00@hotmail.com>
2022-05-06 22:21:40 -0300

    Correção de CORS

--- feedget-server/src/server.ts
+++ feedget-server/src/server.ts
@@ -3,9 +3,7 @@ import { routes } from './routes';
 import cors from 'cors';
 
 const app = express();
-app.use(cors({
-origin: 'http://localhost:3000'
-}));
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
