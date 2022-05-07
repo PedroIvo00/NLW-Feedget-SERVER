7dfd2727daf3925830c72f9429ee089c520a70b7
PedroIvo00 <pedroivo00@hotmail.com>
2022-05-06 21:32:58 -0300

    Ajustes Aula 05
    Ajustes para postgres

--- feedget-server/dist/server.js
+++ feedget-server/dist/server.js
@@ -1,2 +1,17 @@
 "use strict";
-const teste = 1;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const routes_1 = require("./routes");
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: 'http://localhost:3000'
+}));
+app.use(express_1.default.json());
+app.use(routes_1.routes);
+app.listen(3333, () => {
+    console.log('HTTP server running!');
+});
